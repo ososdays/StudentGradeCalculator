023db1538436e5bfbcd70299d764497542b70169
osama <ososarafat88@gmail.com>
2026-09-21 21:01:37 +0300

    Create initial student grade calculator

---
+++ StudentGradeCalculator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string studentName = "Ahmad";
+    int grade1 = 85;
+    int grade2 = 90;
+    int grade3 = 80;
+
+    cout << "Student: " << studentName << endl;
+    cout << "Grade 1: " << grade1 << endl;
+    cout << "Grade 2: " << grade2 << endl;
+    cout << "Grade 3: " << grade3 << endl;
+
+    return 0;
+}
