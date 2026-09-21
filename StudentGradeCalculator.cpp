fbe4211bdd1e314e866eee28a9180489aa7622a5
osama <ososarafat88@gmail.com>
2026-09-21 21:04:20 +0300

    Add student average calculation

--- StudentGradeCalculator.cpp
+++ StudentGradeCalculator.cpp
@@ -2,6 +2,11 @@
 #include <string>
 using namespace std;
 
+double calculateAverage(int grade1, int grade2, int grade3)
+{
+    return (grade1 + grade2 + grade3) / 3.0;
+}
+
 int main()
 {
     string studentName = "Ahmad";
@@ -9,10 +14,13 @@ int main()
     int grade2 = 90;
     int grade3 = 80;
 
+    double average = calculateAverage(grade1, grade2, grade3);
+
     cout << "Student: " << studentName << endl;
     cout << "Grade 1: " << grade1 << endl;
     cout << "Grade 2: " << grade2 << endl;
     cout << "Grade 3: " << grade3 << endl;
+    cout << "Average: " << average << endl;
 
     return 0;
 }
